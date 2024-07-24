1286e125fb0d4d2b85e01518b69131ff538246ab
carlos.guaita <carlos.guaita@udla.edu.ec>
2024-07-24 15:28:59 -0500

    Ejercicio3

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 
 int main (int argc, char *argv[]) {
     printf("Hola mundo");
-    int num1=1, num2=2;
-    int result= num1 + num2;
+    int num1=1, num2=4;
+    int result= num1 + num2
     printf("%d,",result);
-    
+
     return 0;
 }
